a00af229c03543276abad9287b712d376c5c7851
jami-it <abdulrehmanjami73@gmail.com>
2022-05-23 21:56:23 +0500

    JAVA_HELLOw

---
+++ first.java
@@ -0,0 +1,13 @@
+class first{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        a=12;
+        b=12;
+        c=a+b;
+        
+        System.out.println(c);
+        
+        System.out.print("HEllow World");
+    }
+}
