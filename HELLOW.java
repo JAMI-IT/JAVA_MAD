4956ca0e85713050cb6969e2ecf68c2e4a37c29e
jami-it <abdulrehmanjami73@gmail.com>
2022-05-24 23:26:10 +0500

    SUm

--- HELLOW.java
+++ HELLOW.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 class HELLOW{
     public static void main(String args[])
     {
-        int a,b,c;
-        a=12;
+        Scanner sc=new Scanner(System.in);
+        int b,c;
+        //a=12;
         b=12;
-        c=a+b;
+        c=b;
         
         System.out.println(c);
         
         System.out.print("HEllow World");
+        
+        System.out.println("Enter the Number");
+        int a= sc.nextInt();    
+  
     }
 }
---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        a=12;
+        b=21;
+        c=7;
+
+        System.out.println(a+b);
+        System.out.println(a-c);;
+
+
+    }
+}
