239b3019d6f5e8068a41b751650191fc6eca5fb3
JAMI-IT <103955140+JAMI-IT@users.noreply.github.com>
2022-05-28 11:26:09 +0500

    input in java
    input command in java

---
+++ inoput.java
@@ -0,0 +1,12 @@
+class inoput {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("We creat the program for input ");
+        System.out.println("Enter your NAMe :");
+        String s =System.console().readLine();
+        System.out.println(s);
+
+    }
+    
+}
