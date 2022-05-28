e4f45febbee053b17fe7f3c2ad3611121d066afb
jami-it <abdulrehmanjami73@gmail.com>
2022-05-28 11:20:34 +0500

    JAVA

--- inoput.java
+++ inoput.java
@@ -3,7 +3,7 @@ class inoput {
     public static void main(String[] args) 
     {
         System.out.println("We creat the program for input ");
-
+        System.out.println("Enter your NAMe :");
         String s =System.console().readLine();
         System.out.println(s);
 
