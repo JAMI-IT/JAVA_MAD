73fba111fab29a8522cf26ef0c8d636b99db281f
JAMI-IT <103955140+JAMI-IT@users.noreply.github.com>
2022-05-31 22:42:23 +0500

    2darrays

---
+++ Arrrays.java
@@ -0,0 +1,28 @@
+public class Arrrays {
+
+    public static void main(String[] args)
+    {
+        
+         String[] name = {"Ali","Jami"};
+        Integer[][] marks={
+            {12,32,56,46},
+            {234,453,345,34},
+            {23,63,22,64,},
+            {43,54,23,4}
+        };
+
+        System.out.println(marks[2][3]);
+
+    }
+    
+
+
+    // ArrayList<String> namesList=new ArrayList<String>();
+    // namesList.add(e:"123");
+
+    // while
+
+
+    
+
+}
