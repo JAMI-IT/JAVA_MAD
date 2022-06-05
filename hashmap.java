5606bcd180843bc8f39556e77faccad8645f2fa7
jami-it <abdulrehmanjami73@gmail.com>
2022-06-05 12:02:53 +0500

    Arraylist

--- hashmap.java
+++ hashmap.java
@@ -3,9 +3,16 @@ import java.util.HashMap;
 
 public class hashmap {
     public static void main(String[] args) {
-        
+
+       
+     
         HashMap<String,Integer> hm =new HashMap<String,Integer>();
         hm.put("19014156-032", 50);
+        System.out.println("Enter the rollno");
+        System.console().readLine();
+
+        
+
         hm.get("19014156-032");
         System.out.println(hm);
     }
---
+++ task3.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class task3 {
+    public static void main(String[] args) {
+        
+
+        ArrayList<String> car=new ArrayList<String>();
+        car.add("Jami");
+        // car.set(0,"AR");
+        // car.clear();
+        // car.remove(0);
+        car.add("Abdul REhman");
+       
+        System.out.println(car.size());
+        System.out.println(car);
+
+        
+
+
+    }
+}
